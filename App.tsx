89a6db3457e4afbfe2239254a2eeaf55c7c28f04
Gurgen8 <mkrtchyangurgen8@gmail.com>
2023-01-01 23:51:46 +0400

    create custom tab bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useRef } from 'react'
+import React, {useEffect, useReducer, useRef} from 'react';
 import {
   Pressable,
   StatusBar,
@@ -6,23 +6,26 @@ import {
   View,
   Text,
   LayoutChangeEvent,
-} from 'react-native'
-// navigation
-import { NavigationContainer } from '@react-navigation/native'
-import { BottomTabBarProps, BottomTabNavigationOptions, createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  BottomTabBarProps,
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 // svg
-import Svg, { Path } from 'react-native-svg'
+import Svg, {Path} from 'react-native-svg';
 // reanimated
-import Animated, { useAnimatedStyle, withTiming, useDerivedValue } from 'react-native-reanimated'
-// lottie
-import Lottie from 'lottie-react-native'
+import Animated, {
+  useAnimatedStyle,
+  withTiming,
+  useDerivedValue,
+} from 'react-native-reanimated';
+import Lottie from 'lottie-react-native';
+const Tab = createBottomTabNavigator();
 
-// ------------------------------------------------------------------
-
-const Tab = createBottomTabNavigator()
-
-const AnimatedSvg = Animated.createAnimatedComponent(Svg)
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 
 // ------------------------------------------------------------------
 
@@ -31,14 +34,19 @@ const App = () => {
     <>
       <StatusBar barStyle="light-content" />
       <NavigationContainer>
-        <Tab.Navigator
-          tabBar={(props) => <AnimatedTabBar {...props} />}
-        >
+        <Tab.Navigator tabBar={props => <AnimatedTabBar {...props} />}>
           <Tab.Screen
             name="Home"
             options={{
               // @ts-ignore
-              tabBarIcon: ({ ref }) => <Lottie ref={ref} loop={false} source={require('./src/assets/lottie/home.icon.json')} style={styles.icon} />,
+              tabBarIcon: ({ref}) => (
+                <Lottie
+                  ref={ref}
+                  loop={false}
+                  source={require('./src/assets/lottie/home.icon.json')}
+                  style={styles.icon}
+                />
+              ),
             }}
             component={PlaceholderScreen}
           />
@@ -46,7 +54,14 @@ const App = () => {
             name="Upload"
             options={{
               // @ts-ignore
-              tabBarIcon: ({ ref }) => <Lottie ref={ref} loop={false} source={require('./src/assets/lottie/upload.icon.json')} style={styles.icon} />,
+              tabBarIcon: ({ref}) => (
+                <Lottie
+                  ref={ref}
+                  loop={false}
+                  source={require('./src/assets/lottie/upload.icon.json')}
+                  style={styles.icon}
+                />
+              ),
             }}
             component={PlaceholderScreen}
           />
@@ -54,7 +69,14 @@ const App = () => {
             name="Chat"
             options={{
               // @ts-ignore
-              tabBarIcon: ({ ref }) => <Lottie ref={ref} loop={false} source={require('./src/assets/lottie/chat.icon.json')} style={styles.icon} />,
+              tabBarIcon: ({ref}) => (
+                <Lottie
+                  ref={ref}
+                  loop={false}
+                  source={require('./src/assets/lottie/chat.icon.json')}
+                  style={styles.icon}
+                />
+              ),
             }}
             component={PlaceholderScreen}
           />
@@ -62,42 +84,51 @@ const App = () => {
             name="Settings"
             options={{
               // @ts-ignore
-              tabBarIcon: ({ ref }) => <Lottie ref={ref} loop={false} source={require('./src/assets/lottie/settings.icon.json')} style={styles.icon} />,
+              tabBarIcon: ({ref}) => (
+                <Lottie
+                  ref={ref}
+                  loop={false}
+                  source={require('./src/assets/lottie/settings.icon.json')}
+                  style={styles.icon}
+                />
+              ),
             }}
             component={PlaceholderScreen}
           />
         </Tab.Navigator>
       </NavigationContainer>
     </>
-  )
-}
+  );
+};
 
 // ------------------------------------------------------------------
 
 const PlaceholderScreen = () => {
-  return (
-    <View style={{ flex: 1, backgroundColor: '#604AE6' }} />
-  )
-}
+  return <View style={{flex: 1, backgroundColor: '#604AE6'}} />;
+};
 
 // ------------------------------------------------------------------
 
-const AnimatedTabBar = ({ state: { index: activeIndex, routes }, navigation, descriptors } : BottomTabBarProps) => {
-  const { bottom } = useSafeAreaInsets()
+const AnimatedTabBar = ({
+  state: {index: activeIndex, routes},
+  navigation,
+  descriptors,
+}: BottomTabBarProps) => {
+  const {bottom} = useSafeAreaInsets();
 
   // get information about the components position on the screen -----
 
-  const reducer = (state: any, action: { x: number, index: number }) => {
+  const reducer = (state: any, action: {x: number; index: number}) => {
     // Add the new value to the state
-    return [...state, { x: action.x, index: action.index }]
-  }
+    return [...state, {x: action.x, index: action.index}];
+  };
 
-  const [layout, dispatch] = useReducer(reducer, [])
-  console.log(layout)
+  const [layout, dispatch] = useReducer(reducer, []);
+  console.log(layout);
 
   const handleLayout = (event: LayoutChangeEvent, index: number) => {
-    dispatch({ x: event.nativeEvent.layout.x, index })
-  }
+    dispatch({x: event.nativeEvent.layout.x, index});
+  };
 
   // animations ------------------------------------------------------
 
@@ -109,26 +140,25 @@ const AnimatedTabBar = ({ state: { index: activeIndex, routes }, navigation, des
     // We subtract 25 so the active background is centered behind our TabBar Components
     // 20 pixels is the width of the left part of the svg (the quarter circle outwards)
     // 5 pixels come from the little gap between the active background and the circle of the TabBar Components
-    return [...layout].find(({ index }) => index === activeIndex)!.x - 25
+    return [...layout].find(({index}) => index === activeIndex)!.x - 25;
     // Calculate the offset new if the activeIndex changes (e.g. when a new tab is selected)
     // or the layout changes (e.g. when the components haven't finished rendering yet)
-  }, [activeIndex, layout])
+  }, [activeIndex, layout]);
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
       // translateX to the calculated offset with a smooth transition
-      transform: [{ translateX: withTiming(xOffset.value, { duration: 250 }) }],
-    }
-  })
+      transform: [{translateX: withTiming(xOffset.value, {duration: 250})}],
+    };
+  });
 
   return (
-    <View style={[styles.tabBar, { paddingBottom: bottom }]}>
+    <View style={[styles.tabBar, {paddingBottom: bottom}]}>
       <AnimatedSvg
         width={110}
         height={60}
         viewBox="0 0 110 60"
-        style={[styles.activeBackground, animatedStyles]}
-      >
+        style={[styles.activeBackground, animatedStyles]}>
         <Path
           fill="#604AE6"
           d="M20 0H0c11.046 0 20 8.953 20 20v5c0 19.33 15.67 35 35 35s35-15.67 35-35v-5c0-11.045 8.954-20 20-20H20z"
@@ -137,43 +167,48 @@ const AnimatedTabBar = ({ state: { index: activeIndex, routes }, navigation, des
 
       <View style={styles.tabBarContainer}>
         {routes.map((route, index) => {
-          const active = index === activeIndex
-          const { options } = descriptors[route.key]
+          const active = index === activeIndex;
+          const {options} = descriptors[route.key];
 
           return (
             <TabBarComponent
               key={route.key}
               active={active}
               options={options}
-              onLayout={(e) => handleLayout(e, index)}
+              onLayout={e => handleLayout(e, index)}
               onPress={() => navigation.navigate(route.name)}
             />
-          )
+          );
         })}
       </View>
     </View>
-  )
-}
+  );
+};
 
 // ------------------------------------------------------------------
 
 type TabBarComponentProps = {
-  active?: boolean
-  options: BottomTabNavigationOptions
-  onLayout: (e: LayoutChangeEvent) => void
-  onPress: () => void
-}
+  active?: boolean;
+  options: BottomTabNavigationOptions;
+  onLayout: (e: LayoutChangeEvent) => void;
+  onPress: () => void;
+};
 
-const TabBarComponent = ({ active, options, onLayout, onPress }: TabBarComponentProps) => {
+const TabBarComponent = ({
+  active,
+  options,
+  onLayout,
+  onPress,
+}: TabBarComponentProps) => {
   // handle lottie animation -----------------------------------------
-  const ref = useRef(null)
+  const ref = useRef(null);
 
   useEffect(() => {
     if (active && ref?.current) {
       // @ts-ignore
-      ref.current.play()
+      ref.current.play();
     }
-  }, [active])
+  }, [active]);
 
   // animations ------------------------------------------------------
 
@@ -181,30 +216,31 @@ const TabBarComponent = ({ active, options, onLayout, onPress }: TabBarComponent
     return {
       transform: [
         {
-          scale: withTiming(active ? 1 : 0, { duration: 250 })
-        }
-      ]
-    }
-  })
+          scale: withTiming(active ? 1 : 0, {duration: 250}),
+        },
+      ],
+    };
+  });
 
   const animatedIconContainerStyles = useAnimatedStyle(() => {
     return {
-      opacity: withTiming(active ? 1 : 0.5, { duration: 250 })
-    }
-  })
+      opacity: withTiming(active ? 1 : 0.5, {duration: 250}),
+    };
+  });
 
   return (
     <Pressable onPress={onPress} onLayout={onLayout} style={styles.component}>
       <Animated.View
         style={[styles.componentCircle, animatedComponentCircleStyles]}
       />
-      <Animated.View style={[styles.iconContainer, animatedIconContainerStyles]}>
+      <Animated.View
+        style={[styles.iconContainer, animatedIconContainerStyles]}>
         {/* @ts-ignore */}
-        {options.tabBarIcon ? options.tabBarIcon({ ref }) : <Text>?</Text>}
+        {options.tabBarIcon ? options.tabBarIcon({ref}) : <Text>?</Text>}
       </Animated.View>
     </Pressable>
-  )
-}
+  );
+};
 
 // ------------------------------------------------------------------
 
@@ -236,12 +272,12 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   icon: {
     height: 36,
     width: 36,
-  }
-})
+  },
+});
 
 export default App;
